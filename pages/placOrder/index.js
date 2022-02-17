3fd39d4de44f04af26576612d8bda688aaefa593
oliver <894349920@qq.com>
2022-02-17 14:52:17 +0800

    前端代码已经完成 除了“我的”页面中的设置功能未完成，其余功能基本完成。 根据四舍五入定理，前端工作已经完成。 现在只差页面之间的数据传递，与前后端的数据传递就打通项目的任督二脉了

---
+++ pages/placOrder/index.js
@@ -0,0 +1,92 @@
+// pages/placOrder/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        typeList:[
+            {id:0,name:'餐饮',chosed:false},
+            {id:1,name:'文件',chosed:false},
+            {id:2,name:'生鲜',chosed:false},
+            {id:3,name:'蛋糕',chosed:false},
+            {id:4,name:'鲜花',chosed:false},
+            {id:5,name:'钥匙',chosed:false},
+            {id:6,name:'服饰',chosed:false},
+            {id:7,name:'快递',chosed:false},
+            {id:8,name:'其他',chosed:false}
+        ]
+    },
+
+    choiseType(e){
+        // console.log(e)
+
+        let {id} = e.currentTarget.dataset;
+        let temp = JSON.parse(JSON.stringify(this.data.typeList));
+
+        temp.forEach(element => {
+            element.id == id ? element.chosed=true : element.chosed=false;
+        });
+
+        this.setData({
+            typeList:temp
+        })
+        
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
